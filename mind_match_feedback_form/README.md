a4465168159b0b1874ba90bd90361a8dbc50f806
titipata <my.titipat@gmail.com>
2019-09-16 19:14:11 -0400

    Update the URL if request the wrong one

--- mind_match_feedback_form/README.md
+++ mind_match_feedback_form/README.md
@@ -4,5 +4,5 @@ Create a form to get feedback from Mind-Matching event at CCN 2019.
 
 ```
 export FLASK_APP=main.py
-flask run --host=0.0.0.0 --port=6000
+flask run --host=0.0.0.0 --port=5555
 ```
--- mind_match_feedback_form/main.py
+++ mind_match_feedback_form/main.py
@@ -43,13 +43,23 @@ def index():
 
 @app.route('/regid/<reg_id>')
 def feedback_form(reg_id):
-    data = [d for d in mind_match_data 
-            if d['registrant_id'] == int(reg_id)][0]
-    for match in data['matches_info']:
-        match.pop('registrant_id', None)
-    data.update({
-        'enumerate': enumerate,
-    })
+    try:
+        data = [d for d in mind_match_data 
+                if d['registrant_id'] == int(reg_id)][0]
+        for match in data['matches_info']:
+            match.pop('registrant_id', None)
+        data.update({
+            'enumerate': enumerate,
+        })
+    except:
+        data = {
+            "registrant_id": 0,
+            "full_name": "John Doe",
+            "email": "john_doe@gmail.com",
+            "affiliation": "Random University",
+            "matches_info": [],
+            "enumerate": enumerate
+        }
     return flask.render_template('feedback.html', **data)
 
 
