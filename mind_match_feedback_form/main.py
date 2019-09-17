0dffda20b4a7bd96094a741083346f2be569b065
titipata <my.titipat@gmail.com>
2019-09-17 17:53:10 -0400

    Adding enjoyable/ usefulness scale

--- mind_match_feedback_form/main.py
+++ mind_match_feedback_form/main.py
@@ -67,15 +67,20 @@ def feedback_form(reg_id):
 def handle_submit():
     # save data here
     if request.method == 'POST':
+        print(request.form)
         feedback_data = read_json(FEEDBACK_DATA_PATH)
         registrant_id = request.form['registrant_id']
         feedback_text = request.form.get('text_input', '')
         relevances = [request.form.get('relevance_%s' % i, '0')
                       for i in range(0, 6)]
-        satisfactory = [request.form.get(
-            'satisfactory_%s' % i, '0') for i in range(0, 6)]
+        satisfactory = [request.form.get('satisfactory_%s' % i, '0') 
+                        for i in range(0, 6)]
         coi = [request.form.get('coi_%s' % i, '0') for i in range(0, 6)]
         arrange_before = request.form.get('before_checkbox', '0')
+
+        useful = request.form.get('useful', '0')
+        enjoyable = request.form.get('enjoyable', '0')
+
         feedback_data.append({
             'registrant_id': registrant_id,
             'relevances': relevances,
@@ -83,6 +88,8 @@ def handle_submit():
             'coi': coi,
             'feedback_text': feedback_text,
             'arrange_before': arrange_before,
+            'useful': useful,
+            'enjoyable': enjoyable,
             'timestamp': str(datetime.datetime.now())
         })
         save_json(feedback_data, FEEDBACK_DATA_PATH)
